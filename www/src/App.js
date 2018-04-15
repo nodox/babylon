fe77628ed67652c2cb6c57f918f5efc814d7d4bc
nodox <snatera09@gmail.com>
2018-04-14 22:45:57 -0400

    [feat] add the sass support
    Webpack config now has support for sass based css modules.

--- www/src/App.js
+++ www/src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className={styles["App"]}>
+        <h1 className={styles["text"]}>Welcome to React</h1>
+        <header className={styles["App-header"]}>
+          <img src={logo} className={styles["App-logo"]} alt="logo" />
+          <h1 className={styles["App-title"]}>Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className={styles["App-intro"]}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
