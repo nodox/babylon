a4963e0522c3ed7161952a47b3baa976ecac0a25
nodox <snatera09@gmail.com>
2018-04-17 23:43:19 -0400

    add styled components to project

---
+++ www/src/Title.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+// Create a <Title> react component that renders an <h1> which is
+// centered, palevioletred and sized at 1.5em
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: red;
+`;
+
+export default Title
