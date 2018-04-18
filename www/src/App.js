d298feb9307bcfb20bd7401e40a29665b1f71d70
Steven Natera <snatera09@gmail.com>
2018-04-17 22:59:37 -0400

    Revert "[feat] add the sass support"

--- www/src/App.js
+++ www/src/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import styles from './App.scss';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className={styles["App"]}>
-        <h1 className={styles["text"]}>Welcome to React</h1>
-        <header className={styles["App-header"]}>
-          <img src={logo} className={styles["App-logo"]} alt="logo" />
-          <h1 className={styles["App-title"]}>Welcome to React</h1>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className={styles["App-intro"]}>
+        <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
