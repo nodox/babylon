5fb953854588d9f2fedeacfe13ddc25aaa0f321a
Steven Natera <snatera09@gmail.com>
2018-06-10 17:54:29 -0400

    [wip] working example of Github OAuth
    We need to figure out how to do this without the anchor tag, or on the
    client side as there is no way to compare the state strings to prevent
    CSRF.

--- www/src/pages/store/Items.js
+++ www/src/pages/store/Items.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, Route } from "react-router-dom";
+import axios from "axios";
 import daniel from "./daniel.jpg";
 
 import { Grid, Header, Card, Image, Button, Modal } from "semantic-ui-react";
@@ -7,7 +8,15 @@ import { Grid, Header, Card, Image, Button, Modal } from "semantic-ui-react";
 const themes = ["Massively", "Stellar", "Photon"];
 
 class Items extends React.Component {
+
   render() {
+    const state = Math.random().toString(36).slice(2)
+    const scope = 'repo'
+    const client_id = '7f3a16b13129a94ba10e'
+    const redirect_uri = 'http://localhost:5000/api/github/oauth/authorize'
+
+    const githubLink = `https://github.com/login/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope}&state=${state}`
+
     return (
       <div>
         <Grid celled="internally" columns="equal" stackable>
@@ -19,9 +28,9 @@ class Items extends React.Component {
 
           <Grid.Row>
             <Grid.Column>
-              <Button as={Link} to={`${this.props.match.url}/items/new`}>
+              <a href={githubLink} target="_blank">
                 Upload Item
-              </Button>
+              </a>
               <Card.Group centered itemsPerRow={3}>
                 {themes.map((obj, idx) => {
                   return (
