ec454ef6f016bdd69a9aa26deb36d9a0ae9abba1
Steven Natera <snatera09@gmail.com>
2018-06-11 00:27:03 -0400

    better private route

--- www/src/components/PrivateRoute.js
+++ www/src/components/PrivateRoute.js
@@ -1,14 +1,48 @@
-const PrivateRoute = ({ component: Component, authenticated, ...rest }) => {
-  return (
-    <Route
-      {...rest}
-      render={props =>
-        authenticated === true ? (
-          <Component {...props} />
-        ) : (
-          <Redirect from={`${props.match.path}`} to={`/login`} />
-        )
-      }
-    />
-  );
-};
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import axios from "axios";
+
+class PrivateRoute extends React.Component {
+
+  state = {
+    loading: true,
+    authenticated: false
+  }
+
+  checkAuth = () => {
+    const customer = localStorage.getItem('customer')
+    if (!!customer) return true;
+    return false;
+  }
+
+  componentDidMount() {
+    const authenticated = this.checkAuth();
+
+    this.setState({
+      loading: false,
+      authenticated
+    })
+  }
+
+  render() {
+    const { loading, authenticated } = this.state;
+    const { component: Component, ...rest } = this.props;
+
+    // if (loading) return (<Loading />);
+
+    return (
+      <Route
+        {...rest}
+        render={props =>
+          authenticated ? (
+            <Component {...props} />
+          ) : (
+            <Redirect from={`${props.match.path}`} to={`/user/login`} />
+          )
+        }
+      />
+    )
+  }
+}
+
+export { PrivateRoute }
